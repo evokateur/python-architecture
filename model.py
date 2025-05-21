cf81aa679795138bab6f83715acba4caff66a566
ウエスリー <wesley@turnip.evokateur.net>
2025-05-20 22:19:47 -0700

    a flurry of code and all six test (so far) pass

---
+++ model.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional, List, Set
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    order_id: str
+    sku: str
+    quantity: int
+
+
+class Batch:
+    def __init__(self, reference: str, sku: str, quantity: int, eta: Optional[date]):
+        self.reference = reference
+        self.sku = sku
+        self._purchased_quantity = quantity
+        self.eta = eta
+        self._allocations = set()
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    def allocated_quantity(self) -> int:
+        return sum(line.quantity for line in self._allocations)
+
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity()
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity() >= line.quantity
