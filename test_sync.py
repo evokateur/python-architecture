5d532675f5f2fe73903d38cf283613c63fb734c7
Wesley C. Hinkle <evokateur@yahoo.com>
2025-06-05 19:24:56 -0700

    add end to end tests with real filsystem

--- test_sync.py
+++ test_sync.py
@@ -1,4 +1,6 @@
+import tempfile
 from pathlib import Path
+import shutil
 from sync import synchronize_directories, determine_actions
 
 
@@ -56,6 +58,45 @@ class TestE2E:
         assert file_system.list == [
             ("MOVE", Path("/destination/my-file"), Path("/destination/my-file-renamed"))
         ]
+
+    @staticmethod
+    def test_when_a_file_exists_in_the_source_but_not_in_the_destination():
+        source = tempfile.mkdtemp()
+        destination = tempfile.mkdtemp()
+        try:
+            content = "I am a very important file."
+            (Path(source) / "file1.txt").write_text(content)
+
+            synchronize_directories(source, destination)
+
+            expected_path = Path(destination) / "file1.txt"
+            assert expected_path.exists()
+            assert expected_path.read_text() == content
+
+        finally:
+            shutil.rmtree(source)
+            shutil.rmtree(destination)
+
+    @staticmethod
+    def test_when_a_file_has_been_renamed_in_the_source():
+        source = tempfile.mkdtemp()
+        destination = tempfile.mkdtemp()
+        try:
+            content = "I am a file that has been renamed."
+            source_path = Path(source) / "my-file-renamed.txt"
+            old_destination_path = Path(destination) / "file1.txt"
+            expected_destination_path = Path(destination) / "my-file-renamed.txt"
+            source_path.write_text(content)
+            old_destination_path.write_text(content)
+
+            synchronize_directories(source, destination)
+
+            assert old_destination_path.exists() is False
+            assert expected_destination_path.read_text() == content
+
+        finally:
+            shutil.rmtree(source)
+            shutil.rmtree(destination)
 
 
 def test_when_a_file_exists_in_the_source_but_not_in_the_destination():
