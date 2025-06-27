cd2d4d7ca966a1f0571148101bbaeba163e0104f
ウエスリー <wesley@sfizio.evokateur.net>
2025-06-26 20:31:27 -0700

    things typed in but tests not run..

--- conftest.py
+++ conftest.py
@@ -1,8 +1,26 @@
+import time
+from pathlib import Path
+from sqlalchemy import text
+
 import pytest
+import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
 
 from orm import metadata, start_mappers
+import config
+
+
+def wait_for_webapp_to_come_up():
+    deadline = time.time() + 10
+    url = config.get_api_url()
+    print(url)
+    while time.time() < deadline:
+        try:
+            return requests.get(url)
+        except ConnectionError:
+            time.sleep(0.5)
+    pytest.fail("API never came up")
 
 
 @pytest.fixture
@@ -20,5 +38,50 @@ def session(in_memory_db):
 
 
 @pytest.fixture
+def add_stock(session):
+    batches_added = set()
+    skus_added = set()
+
+    postgres_session = session
+
+    def _add_stock(lines):
+        for ref, sku, qty, eta in lines:
+            postgres_session.execute(
+                text(
+                    "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+                    " VALUES (:ref, :sku, :qty, :eta)"
+                ),
+                dict(ref=ref, sku=sku, qty=qty, eta=eta),
+            )
+            [[batch_id]] = postgres_session.execute(
+                text("SELECT id FROM batches WHERE reference=:ref AND sku=:sku"),
+                dict(ref=ref, sku=sku),
+            )
+            batches_added.add(batch_id)
+            skus_added.add(sku)
+        postgres_session.commit()
+
+    yield _add_stock
+
+    for batch_id in batches_added:
+        postgres_session.execute(
+            text("DELETE FROM allocations WHERE batch_id=:batch_id"),
+            dict(batch_id=batch_id),
+        )
+        postgres_session.execute(
+            text("DELETE FROM batches WHERE id=:batch_id"),
+            dict(batch_id=batch_id),
+        )
+    for sku in skus_added:
+        postgres_session.execute(
+            text("DELETE FROM order_lines WHERE sku=:sku"),
+            dict(sku=sku),
+        )
+        postgres_session.commit()
+
+
+@pytest.fixture
 def restart_api():
-    return
+    (Path(__file__).parent / "flask_app.py").touch()
+    time.sleep(0.5)
+    wait_for_webapp_to_come_up()
---
+++ flask_app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
+
+import config
+import model
+import orm
+import repository
+
+orm.start_mappers()
+
+
+get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
+
+app = Flask(__name__)
+
+
+@app.route("/allocate", methods=["POST"])
+def allocate_endpoint():
+    session = get_session()
+    batches = repository.SqlAlchemyRepository(session).list()
+    line = model.OrderLine(
+        request.json["order_id"],
+        request.json["sku"],
+        request.json["quantlty"],
+    )
+    batch_ref = model.allocate(line, batches)
+
+    return jsonify({"batch_ref": batch_ref}), 201
