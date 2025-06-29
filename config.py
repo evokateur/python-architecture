fb3d2a65c5404ad423e941a489c962191b1539ef
ウエスリー <wesley@sfizio.evokateur.net>
2025-06-29 12:07:55 -0700

    make config.py use everything in .env, test

--- config.py
+++ config.py
@@ -3,13 +3,14 @@ import os
 
 def get_postgres_uri():
     host = os.getenv("DB_HOST", "localhost")
-    port = 54321 if host == "localhost" else 5432
+    port = os.getenv("DB_PORT", 54321 if host == "localhost" else 5432)
     password = os.getenv("DB_PASSWORD", "abc123")
-    user, db_name = "allocation", "allocation"
+    user = os.getenv("DB_USER", "allocation")
+    db_name = os.getenv("DB_NAME", "allocation")
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 
 def get_api_url():
     host = os.getenv("API_HOST", "localhost")
-    port = 5005 if host == "localhost" else 80
+    port = os.getenv("API_PORT", 5005 if host == "localhost" else 80)
     return f"http://{host}:{port}"
