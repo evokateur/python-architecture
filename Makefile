dd34079cbbdc79583fc751939b53e8f124093a52
ウエスリー <wesley@sfizio.evokateur.net>
2025-06-28 23:57:33 -0700

    Makefile and conftest.py from upstream

--- Makefile
+++ Makefile
@@ -1,3 +1,17 @@
+all: down build up test
+
+build:
+	docker-compose build
+
+up:
+	docker-compose up -d app
+
+down:
+	docker-compose down
+
+logs:
+	docker-compose logs app | tail -100
+
 test:
 	pytest --tb=short
 
--- conftest.py
+++ conftest.py
@@ -4,23 +4,13 @@ from sqlalchemy import text
 
 import pytest
 import requests
+from requests.exceptions import ConnectionError
+from sqlalchemy.exc import OperationalError
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
 
 from orm import metadata, start_mappers
 import config
-
-
-def wait_for_webapp_to_come_up():
-    deadline = time.time() + 10
-    url = config.get_api_url()
-    print(url)
-    while time.time() < deadline:
-        try:
-            return requests.get(url)
-        except ConnectionError:
-            time.sleep(0.5)
-    pytest.fail("API never came up")
 
 
 @pytest.fixture
@@ -37,12 +27,46 @@ def session(in_memory_db):
     clear_mappers()
 
 
+def wait_for_postgres_to_come_up(engine):
+    deadline = time.time() + 10
+    while time.time() < deadline:
+        try:
+            return engine.connect()
+        except OperationalError:
+            time.sleep(0.5)
+    pytest.fail("Postgres never came up")
+
+
+def wait_for_webapp_to_come_up():
+    deadline = time.time() + 10
+    url = config.get_api_url()
+    while time.time() < deadline:
+        try:
+            return requests.get(url)
+        except ConnectionError:
+            time.sleep(0.5)
+    pytest.fail("API never came up")
+
+
+@pytest.fixture(scope="session")
+def postgres_db():
+    engine = create_engine(config.get_postgres_uri())
+    wait_for_postgres_to_come_up(engine)
+    metadata.create_all(engine)
+    return engine
+
+
 @pytest.fixture
-def add_stock(session):
+def postgres_session(postgres_db):
+    start_mappers()
+    yield sessionmaker(bind=postgres_db)()
+    clear_mappers()
+
+
+@pytest.fixture
+def add_stock(postgres_session):
     batches_added = set()
     skus_added = set()
-
-    postgres_session = session
 
     def _add_stock(lines):
         for ref, sku, qty, eta in lines:
