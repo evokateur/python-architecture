4711bcd48724e1d833722fa495ac92b3e5b9cfb6
ウエスリー <wesley@sfizio.evokateur.net>
2025-06-29 16:34:14 -0700

    let config.py use POSTGRES_URI or API_URL (simpler) if available, otherwise gin them up, as it does

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 
-def get_postgres_uri():
+def construct_postgres_uri():
     host = os.getenv("DB_HOST", "localhost")
     port = os.getenv("DB_PORT", 54321 if host == "localhost" else 5432)
     password = os.getenv("DB_PASSWORD", "abc123")
@@ -10,7 +10,15 @@ def get_postgres_uri():
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 
-def get_api_url():
+def get_postgres_uri():
+    return os.getenv("POSTGRES_URI", construct_postgres_uri())
+
+
+def construct_api_url():
     host = os.getenv("API_HOST", "localhost")
     port = os.getenv("API_PORT", 5005 if host == "localhost" else 80)
     return f"http://{host}:{port}"
+
+
+def get_api_url():
+    return os.getenv("API_URL", construct_api_url())
