b8abdcde80812fd766d13d14df2e07fb65f3a416
Wesley C. Hinkle <wchinkle@gmail.com>
2025-06-30 12:45:28 -0700

    oops

--- model.py
+++ model.py
@@ -74,7 +74,7 @@ def allocate(line: OrderLine, batches: List[Batch]) -> str:
     try:
         batch = next(batch for batch in sorted(batches) if batch.can_allocate(line))
     except StopIteration:
-        raise OutOfStock(f"Out of stock for {line}")
+        raise OutOfStock(f"Out of stock for {line.sku}")
     else:
         batch.allocate(line)
         return batch.reference
