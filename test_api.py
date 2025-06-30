c5c9f2d4be9eab80de9724e6b8f8384e6cc398f2
Wesley C. Hinkle <wchinkle@gmail.com>
2025-06-30 14:31:17 -0700

    test only unhappy and happy paths of api

--- test_api.py
+++ test_api.py
@@ -22,7 +22,7 @@ def random_order_id(name=""):
 
 
 @pytest.mark.usefixtures("restart_api")
-def test_api_returns_allocation(add_stock):
+def test_happy_path_returns_201_and_allocated_batch(add_stock):
     sku, other_sku = random_sku(), random_sku("other")
     early_batch = random_batch_ref("1")
     later_batch = random_batch_ref("2")
@@ -42,47 +42,7 @@ def test_api_returns_allocation(add_stock):
 
 
 @pytest.mark.usefixtures("restart_api")
-def test_allocations_are_persisted(add_stock):
-    sku = random_sku()
-    batch1, batch2 = random_batch_ref("1"), random_batch_ref("2")
-    order1, order2 = random_order_id("1"), random_order_id("2")
-    add_stock([(batch1, sku, 10, "2025-01-01"), (batch2, sku, 10, "2025-01-02")])
-    line1 = {"order_id": order1, "sku": sku, "quantity": 10}
-    line2 = {"order_id": order2, "sku": sku, "quantity": 10}
-    url = config.get_api_url()
-
-    # first order uses up all stock in batch1
-    r = requests.post(f"{url}/allocate", json=line1)
-    assert r.status_code == 201
-    assert r.json()["batch_ref"] == batch1
-
-    # second order should go to batch2
-    r = requests.post(f"{url}/allocate", json=line2)
-    assert r.status_code == 201
-    assert r.json()["batch_ref"] == batch2
-
-
-@pytest.mark.usefixtures("restart_api")
-def test_400_message_for_out_of_stock(add_stock):
-    sku = random_sku()
-    small_batch = random_batch_ref()
-    large_order = random_order_id()
-
-    add_stock(
-        [
-            (small_batch, sku, 10, "2025-01-01"),
-        ]
-    )
-
-    data = {"order_id": large_order, "sku": sku, "quantity": 20}
-    url = config.get_api_url()
-    r = requests.post(f"{url}/allocate", json=data)
-    assert r.status_code == 400
-    assert r.json()["message"] == f"Out of stock for {sku}"
-
-
-@pytest.mark.usefixtures("restart_api")
-def test_400_message_for_invalid_sku(add_stock):
+def test_unhappy_path_returns_400_and_error_message(add_stock):
     unknown_sku, order_id = random_sku(), random_order_id()
     data = {"order_id": order_id, "sku": unknown_sku, "quantity": 20}
     url = config.get_api_url()
