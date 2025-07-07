3dc1b2cdbb02a56b72f0c092597466bc67213c3b
ウエスリー <wesley@sfizio.evokateur.net>
2025-07-07 05:49:56 -0700

    untangle add_stock fixture from api test, lose add_stock fixture

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -22,31 +22,33 @@ def random_order_id(name=""):
 
 
 @pytest.mark.usefixtures("restart_api")
-def test_happy_path_returns_201_and_allocated_batch(add_stock):
+def test_happy_path_returns_201_and_allocated_batch():
     sku, other_sku = random_sku(), random_sku("other")
     early_batch = random_batch_ref("1")
     later_batch = random_batch_ref("2")
     other_batch = random_batch_ref("3")
-    add_stock(
-        [
-            (later_batch, sku, 100, "2025-01-02"),
-            (early_batch, sku, 100, "2025-01-01"),
-            (other_batch, other_sku, 100, None),
-        ]
-    )
+
+    post_to_add_batch(later_batch, sku, 100, "2025-01-02")
+    post_to_add_batch(early_batch, sku, 100, "2025-01-01")
+    post_to_add_batch(other_batch, other_sku, 100, None)
+
     data = {"order_id": random_order_id(), "sku": sku, "quantity": 3}
+
     url = config.get_api_url()
     r = requests.post(f"{url}/allocations", json=data)
+
     assert r.status_code == 201
     assert r.json()["batch_ref"] == early_batch
 
 
 @pytest.mark.usefixtures("restart_api")
-def test_unhappy_path_returns_400_and_error_message(add_stock):
+def test_unhappy_path_returns_400_and_error_message():
     unknown_sku, order_id = random_sku(), random_order_id()
     data = {"order_id": order_id, "sku": unknown_sku, "quantity": 20}
+
     url = config.get_api_url()
     r = requests.post(f"{url}/allocations", json=data)
+
     assert r.status_code == 400
     assert r.json()["message"] == f"Invalid sku {unknown_sku}"
 
