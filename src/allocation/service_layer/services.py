c45a283a27a0bc54d657029ba3c9daf86587c6ef
ウエスリー <wesley@cipolla.evokateur.net>
2025-10-12 13:41:02 -0700

    services using uow, services tests passing

--- src/allocation/service_layer/services.py
+++ src/allocation/service_layer/services.py
@@ -1,5 +1,5 @@
 from allocation.domain import model
-from allocation.adapters.repository import AbstractRepository
+from allocation.service_layer.unit_of_work import AbstractUnitOfWork
 
 
 class InvalidSku(Exception):
@@ -18,39 +18,42 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def add_batch(
-    reference: str, sku: str, quantity: int, eta, repo: AbstractRepository, session
-):
+def add_batch(reference: str, sku: str, quantity: int, eta, uow: AbstractUnitOfWork):
     batch = model.Batch(reference, sku, quantity, eta)
-    repo.add(batch)
-    session.commit()
+    with uow:
+        uow.batches.add(batch)
+        uow.commit()
 
 
-def allocate(order_id, sku, quantity, repo: AbstractRepository, session) -> str:
+def allocate(order_id, sku, quantity, uow: AbstractUnitOfWork) -> str:
     order_line = model.OrderLine(order_id, sku, quantity)
 
-    batches = repo.list()
-    if not is_valid_sku(order_line.sku, batches):
-        raise InvalidSku(f"Invalid sku {order_line.sku}")
+    with uow:
+        batches = uow.batches.list()
+        if not is_valid_sku(order_line.sku, batches):
+            raise InvalidSku(f"Invalid sku {order_line.sku}")
 
-    try:
-        batch_ref = model.allocate(order_line, batches)
-    except model.OutOfStock as e:
-        raise OutOfStock(str(e))
+        try:
+            batch_ref = model.allocate(order_line, batches)
+        except model.OutOfStock as e:
+            raise OutOfStock(str(e))
 
-    session.commit()
+        uow.commit()
+
     return batch_ref
 
 
-def deallocate(order_id, sku, quantity, repo: AbstractRepository, session) -> str:
+def deallocate(order_id, sku, quantity, uow: AbstractUnitOfWork) -> str:
     order_line = model.OrderLine(order_id, sku, quantity)
 
-    batches = repo.list()
+    with uow:
+        batches = uow.batches.list()
 
-    try:
-        batch_ref = model.deallocate(order_line, batches)
-    except model.DeallocationError as e:
-        raise DeallocationError(str(e))
+        try:
+            batch_ref = model.deallocate(order_line, batches)
+        except model.DeallocationError as e:
+            raise DeallocationError(str(e))
 
-    session.commit()
+        uow.commit()
+
     return batch_ref
